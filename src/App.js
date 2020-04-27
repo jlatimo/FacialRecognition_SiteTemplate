fa12f2b0cfe4c6be10925475b7028d5630846ffa
Justin Latimore <Justice1>
2020-04-26 22:26:57 -0400

    Finished UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+// import HomePage from './HomePage';
 import Particles from 'react-particles-js';
 import SignInForm from './Components/SignIn/SignInForm';
 import Navigation from './Components/Navigation/Navigation';
-import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
 import Register from './Components/Registration/Register';
 import ImageLinkForm from './Components/LinkForm/ImageLinkForm';
 import FaceRecognition from './Components/FacialRecognition/FaceRecognition';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 const Clarifai = require('clarifai');
@@ -33,9 +34,26 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box:{},
+      box: {},
       route: 'SignInForm'
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.boundingg_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  faceBox = (box) => {
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
@@ -44,7 +62,7 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
-    this.setState({route:route});
+    this.setState({ route: route });
   }
 
   onSubmit = () => {
@@ -53,36 +71,28 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-        function (response) {
-          console.log('Response!', response)
-        },
-        function (err) {
-          console.log('Error!', err)
-        }
-      );
+      .then(response => this.faceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log('Error!', err));
   }
 
   render() {
     return (
-      <div className="App">
-        <Particles className='particles' 
-        params={particleOptions} 
-        />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        {this.state.route === 'SignInForm'
-        ?<SignInForm onRouteChange={this.onRouteChange}/>
-          :<div>
-            <Logo/>
-            <Rank/>
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onSubmit={this.onSubmit}
-            />
-            <FaceRecognition imageUrl={this.state.imageUrl} />
-          </div>
-        }
-      </div>
+      <Router>
+        <div className="App">
+          <Particles className='particles'
+            params={particleOptions}
+          />
+
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={SignInForm} />
+            <Route path="/rank" component={Rank} />
+            <Route path="/register" component={Register} />
+            <Route path="/Linkform" component={ImageLinkForm} />
+            <Route path="/facialRecognition" component={FaceRecognition} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Tilt from 'react-tilt'
+import PokeBall from './fight-pokemon-96.png';
+import './LogoNav.css';
+import {Link} from "react-router-dom";
 
-const Navigation = ({onRouteChange}) =>{
+const Navigation = ({ onRouteChange }) => {
 
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p onClick={() => onRouteChange('SignInForm')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
+        <div >
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Link to='/'>
+            <Tilt className=" mt2 Tilt shadow-2" options={{ max: 30 }} style={{ height: 150, width: 150 }} >
+                    <div className="Tilt-inner pa3">
+                        <img alt='logo' style={{ paddingTop: '5px' }} src={PokeBall} />
+                    </div>
+                </Tilt>
+            </Link>
+                
+            <Link to='/'>
+            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>  </Link>
+            </nav>
+        </div>
     );
 }
 
